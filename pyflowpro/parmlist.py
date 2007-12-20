5c2a2bf32ce3bb71f1d7f903b26a2e733300b7e2
Matthew Scott <gldnspud@gmail.com>
2007-12-19 23:28:56 -0800

    Parmlist  - Allow initialization from parmlist-formatted strings.

--- pyflowpro/parmlist.py
+++ pyflowpro/parmlist.py
@@ -1,10 +1,18 @@
 class Parmlist(dict):
 
-    def __init__(self, **kw):
+    def __init__(self, *args, **kw):
         super(Parmlist, self).__init__()
-        # Manually apply keyword args so they pass through __setitem__.
-        for key, value in kw.iteritems():
-            self[key] = value
+        # >0 positional args with >0 keyword args is ambiguous.
+        if len(args) > 0 and len(kw) > 0:
+            raise TypeError(
+                'Provide either positional or keyword arguments, not both.')
+        # >1 positional args is ambiguous.
+        if len(args) > 1:
+            raise TypeError('Provide only one positional argument.')
+        if len(args) == 1:
+            # Positional arg: convert to a dictionary.
+            kw = _to_dict(args[0])
+        self.update(kw)
 
     def __setitem__(self, key, value):
         if not isinstance(key, str):
@@ -30,3 +38,80 @@ class Parmlist(dict):
             args.append('%s=%s' % (key, value))
         args.sort()
         return '"' + '&'.join(args) + '"'
+
+    def update(self, other):
+        # Update item-by-item so that __setitem__ rules are enforced.
+        for key, value in other.iteritems():
+            self[key] = value
+        
+
+BEGIN, NAME, LEN, VALUE, RESET, END = range(6)
+
+
+def _to_dict(s):
+    """Return a dictionary version of the parmlist string `s`."""
+    # Not a very clever implementation, but used in production for a
+    # couple of years, and not out to be a speed demon anyway.  Just a
+    # simple state machine.  If anyone takes the time to implement a
+    # regex version, please let me know!  -mscott
+    result = {}
+    state = BEGIN
+    cur_name = ''
+    cur_len_str = ''
+    cur_len = 0
+    cur_value = ''
+    processed = ''
+    for c in s:
+        processed += c
+        if state is BEGIN:
+            if c is not '"':
+                raise ValueError('String must begin with a double quote.')
+            state = NAME
+        elif state is NAME:
+            if c.isalpha():
+                cur_name += c
+            elif c in '[]':
+                state = LEN
+            elif c is '=':
+                state = VALUE
+            else:
+                raise ValueError('Error parsing string into values at %r.'
+                                 % processed)
+        elif state is LEN:
+            if c.isdigit():
+                cur_len_str += c
+            elif c is ']':
+                state = NAME
+            else:
+                raise ValueError('Error parsing string into values at %r.'
+                                 % processed)
+        elif state is VALUE:
+            if cur_len_str:
+                cur_len = int(cur_len_str)
+                cur_len_str = ''
+            if cur_len:
+                cur_value += c
+                cur_len -= 1
+                if not cur_len:
+                    state = RESET
+                    continue
+            elif c is '&':
+                state = RESET
+            elif c is '"':
+                state = END
+            else:
+                cur_value += c
+        elif state is RESET:
+            if c is '"':
+                state = END
+        if state is RESET or state is END:
+            result[cur_name] = cur_value
+            cur_name = ''
+            cur_len_str = ''
+            cur_len = 0
+            cur_value = ''
+            if state is RESET:
+                state = NAME
+    if state is not END:
+        raise ValueError('Premature end of string.')
+    return result
--- pyflowpro/tests/test_parmlist.py
+++ pyflowpro/tests/test_parmlist.py
@@ -7,7 +7,34 @@ from pyflowpro.parmlist import Parmlist
 
 class TestParmlist(object):
 
-    def test_string(self):
+    def test_fromstring(self):
+        parms = Parmlist(
+            '"'
+            'ACCT=5555444433332222&'
+            'AMT=123.00&'
+            'EXPDATE=0308&'
+            'PARTNER=PayPal&'
+            'PWD[6]=x1y&=3&'
+            'TENDER=C&'
+            'TRXTYPE=S&'
+            'USER=SuperMerchant&'
+            'VENDOR=SuperMerchant'
+            '"'
+            )
+        expected = Parmlist(
+            TRXTYPE='S',
+            TENDER='C',
+            PARTNER='PayPal',
+            VENDOR='SuperMerchant',
+            USER='SuperMerchant',
+            PWD='x1y&=3',
+            ACCT='5555444433332222',
+            EXPDATE='0308',
+            AMT='123.00',
+            )
+        assert parms == expected
+
+    def test_tostring(self):
         """`Parmlist.__str__` returns a parmlist-formatted string
         based on values in a dictionary."""
         parms = Parmlist(
